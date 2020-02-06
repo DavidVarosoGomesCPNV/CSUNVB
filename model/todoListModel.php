b5105d9b9f2a55b8342904a8dec85378f8f41214
ValZ <Valentin.ZINGG@cpnv.ch>
2020-02-06 15:34:39 +0100

    Ajout lecture de taches & affichage

--- model/todoListModel.php
+++ model/todoListModel.php
@@ -1,35 +1,24 @@
 <?php
 /**
- * Ce cartouche vaudra quelques points en moins au groupe qui osera le laisser là tel quel ...
- * Auteur: X. Carrel
+ * Auteur: Valentin Zingg
  * Date: Février 2020
  **/
 
-/**
- * Retourne tous les items dans un tableau indexé de tableaux associatifs
- * Des points seront également retirés au groupe qui osera laisser une des fonctions de ce fichier telle quelle
- * sans l'adapter au niveau de son nom et de son code pour qu'elle dise plus précisément de quelles données elle traite
- */
 function readTodoListItems()
 {
-    return json_decode(file_get_contents("model/dataStorage/items.json"),true);
+    $data = file_get_contents("model/dataStorage/todos.json");
+    $vraijson = json_decode($data,true);
+    return $vraijson;
 }
 
-/**
- * Retourne un item précis, identifié par son id
- * ...
- */
-function readTodoListItem($id)
+
+function searchTodoListItems($id)
 {
-    $items = getTodoListItems();
+    $items = readTodoListItems();
     // TODO: coder la recherche de l'item demandé
-    return $item;
+    return $items;
 }
 
-/**
- * Sauve l'ensemble des items dans le fichier json
- * ...
- */
 function updateTodoListItems($items)
 {
     file_put_contents("model/dataStorage/items.json",json_encode($items));
--- view/todoListHome.php
+++ view/todoListHome.php
@@ -1,10 +1,45 @@
 <?php
+include_once 'model/todoListModel.php';
 ob_start();
 $title = "CSU-NVB - Tâches hebdomadaires";
 ?>
-<div class="row m-2">
-    <h1>Tâches hebdomadaires</h1>
+
+<div class="container">
+    <div class="row m-2">
+        <h1>Tâches hebdomadaires</h1>
+    </div>
+    <hr>
+    <div class="row m-2">
+        <h2>Tâches</h2>
+    </div>
+    <hr>
+    <div class="row col-m">
+        <?php
+        $tododata = readTodoListItems();
+        foreach($tododata as $truc)
+        {
+            echo("<div class='tache'>");
+            echo("<h2>".$truc["description"]."</h2>");
+            echo("Base : ".$truc["base"]."<br>");
+            if($truc["nightjob"] != 0)
+            {
+                echo("Job de nuit : Oui"."<br>");
+            }
+            else
+            {
+                echo("Job de nuit : Non"."<br>");
+            }
+            echo("Date : ".$truc["date"]."<br>");
+            echo("Date : ".$truc["id"]."<br>");
+            echo("<a class='btn btn-primary'>Voir tâche</a>");
+            echo("</div>");
+        }
+        ?>
+    </div>
 </div>
+
+
+
 <?php
 $content = ob_get_clean();
 require "gabarit.php";
